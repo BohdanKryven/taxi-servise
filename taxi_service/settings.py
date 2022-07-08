7d10eeb766c8da1013802e24dd564dc539ee5140
bohdan <bodyan1108@gmail.com>
2022-07-08 19:13:22 +0300

    Added DATABASE_URL setting

--- taxi_service/settings.py
+++ taxi_service/settings.py
@@ -10,6 +10,8 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / "subdir".
+import dj_database_url
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
@@ -87,6 +89,9 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+db_from_env = dj_database_url.config(conn_max_age=500)  # DATABASE_URL
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
